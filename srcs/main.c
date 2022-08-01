5ed47379df92fd75f3e69db4f395b1fee8d9d877
Pedro Paulo <pe.pn03@gmail.com>
2022-08-01 12:49:26 -0300

    Removed test message from main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 20:35:25 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/07/31 18:47:08 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/08/01 12:39:26 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int	main(int argc, char *argv[])
 	processes = ft_lstsize(data);
 	for (int i = 0; i < processes; i++)
 		wait(NULL);
-	ft_printf("xxxxxxxxxxxxxxxx\n"); // test purpose
 	ft_lstclear(&data, clean_data);
 	close(output_fd);
 	if (status)
