0995201d7cc9f3acdb5d6e362acd3e48bea95cd2
Pedro Paulo <pe.pn03@gmail.com>
2022-07-31 23:46:43 -0300

    PING protocol working in loop. Fixed http message. Program stops when address is invalid (only works for PING and HTTP)

--- srcs/format.c
+++ srcs/format.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 15:15:50 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/07/29 17:45:46 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/07/31 23:30:13 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 void	format_out_ping(char *line, t_data content, int output)
 {
-	char *time;
+	char	*time;
+	char	*packets;
 	time = ft_strnstr(line, "time", ft_strlen(line));
-	if (time)
+	packets = ft_strnstr(line, "packets", ft_strlen(line));
+	if (packets)
+	{
+		ft_printf("# %s\n", packets);
+		dprintf(output, "# %s", packets);
+	}
+	else if (time)
 	{
 		ft_printf("# Name: %s\tProtocol: %s\t%s",
 		content.name, content.protocol, time);
@@ -32,19 +39,21 @@ void	format_out_http(char *line, t_data content, int output)
 
 	line_splt = ft_split(line, ' ');
 	code = ft_atoi(line_splt[1]);
-	ft_printf("# Name: %s\t Protcol: %s\t Request: %s\t code_expected: %d\tgot: %d\n",
+	ft_printf("# Name: %s\t Protocol: %s\t Request: %s\t code_expected: %d\tgot: %d\n\n",
 	content.name, content.protocol, content.http_method, content.http_code, code);
-	dprintf(output, "# Name: %s\t Protcol: %s\t Request: %s\t code_expected: %d\tgot: %d\n",
+	dprintf(output, "# Name: %s\t Protocol: %s\t Request: %s\t code_expected: %d\tgot: %d\n",
 	content.name, content.protocol, content.http_method, content.http_code, code);
 	clean_array((void **)line_splt);
 }
 
 void	format_out_dns(char *line, t_data content, int output)
 {
-	ft_printf("# Name: %s\tProtocol: %s\tAddress: %s\tDNS_server: %s\n# Got: %s\n",
-	content.name, content.protocol, content.address, content.dns_server, line);
-	dprintf(output, "# Name: %s\tProtocol: %s\tAddress: %s\tDNS_server: %s\n# Got: %s\n",
-	content.name, content.protocol, content.address, content.dns_server, line);
+	if (line){
+		ft_printf("# Name: %s\tProtocol: %s\tAddress: %s\tDNS_server: %s\n# Got: %s\n",
+		content.name, content.protocol, content.address, content.dns_server, line);
+		dprintf(output, "# Name: %s\tProtocol: %s\tAddress: %s\tDNS_server: %s\n# Got: %s\n",
+		content.name, content.protocol, content.address, content.dns_server, line);
+	}
 }
 
 void	print_menu(int output)
--- srcs/protocols.c
+++ srcs/protocols.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 21:10:33 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/07/31 22:57:59 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/07/31 23:43:35 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,30 @@ int	start_ping(t_data data, int output)
 	int		pid;
 	char	*line;
 
-	if (pipe(pipes) == -1)
-		return (EPIPE);
-	pid = fork();
-	if (pid == -1)
-		return (EFORK);
-	else if (pid == 0){
-		close(pipes[0]);
-		dup2(pipes[1], 1);
+	while (1){
+		if (pipe(pipes) == -1)
+			return (EPIPE);
+		pid = fork();
+		if (pid == -1)
+			return (EFORK);
+		else if (pid == 0){
+			close(pipes[0]);
+			dup2(pipes[1], 1);
+			close(pipes[1]);
+			execlp("ping", "ping", data.address, "-c", "1", NULL);
+		}
 		close(pipes[1]);
-		execlp("ping", "ping", data.address, "-i", ft_itoa(data.interval), NULL);
-	}
-	close(pipes[1]);
-	line = get_next_line(pipes[0]);
-	while (line){
-		write(output, line, ft_strlen(line));
-		write(output, "\n", 1);
-		format_out_ping(line, data, output);
-		free(line);
 		line = get_next_line(pipes[0]);
+		if (!line)
+			return (EXIT_FAILURE);
+		while (line){
+			write(output, line, ft_strlen(line));
+			write(output, "\n", 1);
+			format_out_ping(line, data, output);
+			free(line);
+			line = get_next_line(pipes[0]);
+		}
+		sleep(data.interval);
 	}
 	return (EXIT_SUCCESS);
 }
@@ -63,7 +68,8 @@ int	start_http(t_data data, int output)
 		}
 		close(pipes[1]);
 		line = get_next_line(pipes[0]);
-		ft_printf("%s", data.http_method);
+		if (!line)
+			return (-1);
 		while (line){
 			write(output, line, ft_strlen(line));
 			if (ft_strnstr(line, "HTTP/", ft_strlen(line)))
@@ -85,7 +91,7 @@ int	start_dns(t_data data, int output)
 	int		pipes[2];
 	char	*line;
 	int		pid;
-	char	*line_out;
+	char	*line_out = NULL;
 	
 	while (1){
 		if (pipe(pipes) == -1)
@@ -109,7 +115,8 @@ int	start_dns(t_data data, int output)
 			line = get_next_line(pipes[0]);
 		}
 		format_out_dns(line_out, data, output);
-		free(line_out);
+		if (line_out)
+			free(line_out);
 		close(pipes[0]);
 		sleep(data.interval);
 	}
